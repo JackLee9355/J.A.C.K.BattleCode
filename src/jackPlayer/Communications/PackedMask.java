a9d5ba4ea8f9f69075f3b79597b79e37d92aa3a0
Jack Lee <welsar55@gmail.com>
2023-01-10 16:17:53 -0600

    Fix broken page index mask

--- src/jackPlayer/Communications/PackedMask.java
+++ src/jackPlayer/Communications/PackedMask.java
@@ -5,7 +5,7 @@ public enum PackedMask {
     WELL_TYPE(0x7000, 12),
     WELL_X(0x0FC0, 6),
     WELL_Y(0x003F, 0),
-    PAGE_INDEX(0x800, 15);
+    PAGE_INDEX(0xF000, 12);
 
     public final int mask;
     public final int shift;
