0cc93ace8eced82bd58de50fcf87f09b8443892a
Katya Borisova <c.b.borisova@gmail.com>
2023-01-10 16:24:29 -0600

    general explore

--- src/jackPlayer/RobotPlayer.java
+++ src/jackPlayer/RobotPlayer.java
@@ -54,4 +54,67 @@ public strictfp class RobotPlayer {
             }
         }
     }
-}
+
+    public static void generalExplore(RobotController rc) throws GameActionException {
+        Direction dir = null;
+        int[] nearby = new int[4]; //0-N (up) 1-E (right), 2-S (down), 3-W (left)
+        // +1 for ally robot (to disperse), +1 for opposite direction if enemy robot (to avoid)
+        RobotInfo[] nearbyRobots = rc.senseNearbyRobots();
+        int thisX = rc.getLocation().x;
+        int thisY = rc.getLocation().y;
+        for (RobotInfo robot : nearbyRobots) {
+            int x = robot.location.x;
+            int y = robot.location.y;
+            int diffX = x - thisX; //pos = enemy is N, neg = enemy = S
+            int diffY = y - thisY; //pos = enemy is E, neg = W
+            if (diffX > 0) {
+                nearby[0] += 1;
+            }
+            if (diffX < 0) {
+                nearby[2] += 1;
+            }
+            if (diffY > 0) {
+                nearby[1] += 1;
+            }
+            if (diffY < 0) {
+                nearby[3] += 1;
+            }
+        }
+        boolean N = false;
+        boolean S = false;
+        boolean E = false;
+        boolean W = false;
+        int updown = nearby[0] - nearby[2];
+        int ew = nearby[1] - nearby[3];
+        if (updown > 0) {
+            S = true; //enemies in N
+        } else if (updown < 0) {
+            N = true;
+        }
+        if (ew > 0) {
+            W = true;
+        }
+        if (ew < 0) {
+            E = true;
+        }
+        if(N && E && rc.canMove(Direction.NORTHEAST)){
+            dir = Direction.NORTHEAST;
+        } else if (N && W && rc.canMove((Direction.NORTHWEST))){
+            dir = Direction.NORTHWEST;
+        } else if (S && E && rc.canMove((Direction.SOUTHEAST))){
+            dir = Direction.SOUTHEAST;
+        } else if (S && W && rc.canMove(Direction.SOUTHWEST)){
+            dir = Direction.SOUTHWEST;
+        } else if(N && rc.canMove(Direction.NORTH)){
+            dir = Direction.NORTH;
+        } else if (E && rc.canMove(Direction.EAST)){
+            dir = Direction.EAST;
+        } else if (S && rc.canMove(Direction.SOUTH)){
+            dir = Direction.SOUTH;
+        } else if(W && rc.canMove((Direction.WEST)){
+            dir = Direction.WEST;
+        }
+        if(dir != null){
+            rc.move(dir);
+        }
+    }}
