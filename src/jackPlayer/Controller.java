0a12d9389e7857b9c54265451b2e085603ae6614
Andy <hoyle020@umn.edu>
2023-01-10 16:55:04 -0600

    Merged with collin's branch

--- src/jackPlayer/Controller.java
+++ src/jackPlayer/Controller.java
@@ -1,18 +1,19 @@
 package jackPlayer;
 
-<<<<<<< HEAD
 import battlecode.common.*;
-=======
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
->>>>>>> 0a0147ef05dd5b52658d5771718ff14b78e3d8eb
 
 import java.util.Random;
 
 public abstract class Controller {
     protected int turnCount = 0;
+    protected final int mapWidth;
+    protected final int mapHeight;
+    protected MapLocation myLocation;
+    protected final int[] sharedArray = new int[64];
     protected static Direction alongObstacleDir = null;
     protected static final Random rng = new Random(6147);
     protected static final Direction[] directions = {
@@ -26,8 +27,21 @@ public abstract class Controller {
             Direction.NORTHWEST,
     };
 
-<<<<<<< HEAD
-    public abstract void run(RobotController rc) throws GameActionException;
+    public Controller(RobotController rc) {
+        mapWidth = rc.getMapWidth();
+        mapHeight = rc.getMapHeight();
+    }
+
+    public void run(RobotController rc) throws GameActionException {
+        turnCount++;
+        myLocation = rc.getLocation();
+    }
+
+    protected void readEntireArray(RobotController rc) throws GameActionException {
+        for (int i = 0; i < 64; i++) {
+            sharedArray[i] = rc.readSharedArray(i);
+        }
+    }
 
     public static MapLocation[] enemiesInVision(RobotController rc, int visionRadiusSquared, Team enemyTeam) throws GameActionException {
         RobotInfo[] enemies = rc.senseNearbyRobots(visionRadiusSquared, enemyTeam);
@@ -76,30 +90,4 @@ public abstract class Controller {
 
         return false;
     }
-=======
-    protected final int mapWidth;
-    protected final int mapHeight;
-
-    protected MapLocation myLocation;
-    protected final int[] sharedArray = new int[64];
-
-    public Controller(RobotController rc) {
-        mapWidth = rc.getMapWidth();
-        mapHeight = rc.getMapHeight();
-    }
-
-    public void run(RobotController rc) throws GameActionException {
-        turnCount++;
-        myLocation = rc.getLocation();
-
-    }
-
-    protected void readEntireArray(RobotController rc) throws GameActionException {
-        for (int i = 0; i < 64; i++) {
-            sharedArray[i] = rc.readSharedArray(i);
-        }
-    }
-
-
->>>>>>> 0a0147ef05dd5b52658d5771718ff14b78e3d8eb
 }
