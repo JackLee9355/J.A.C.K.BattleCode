d5906d7c7ad153ff5f8625be22953b7e5442d51d
Jack Lee <welsar55@gmail.com>
2023-01-11 17:43:32 -0600

    Fix resource deposits

--- src/jackPlayer/CarrierController.java
+++ src/jackPlayer/CarrierController.java
@@ -64,7 +64,18 @@ public class CarrierController extends Controller {
 
     private void attemptDeposit(RobotController rc) throws GameActionException {
         if (headquarter.isAdjacentTo(rc.getLocation())) {
-            rc.transferResource(headquarter, wellType, rc.getResourceAmount(wellType));
+            int exAmount = rc.getResourceAmount(ResourceType.ELIXIR);
+            if (exAmount > 0) {
+                rc.transferResource(headquarter, ResourceType.ELIXIR, exAmount);
+            }
+            int adAmount = rc.getResourceAmount(ResourceType.ADAMANTIUM);
+            if (adAmount > 0) {
+                rc.transferResource(headquarter, ResourceType.ADAMANTIUM, adAmount);
+            }
+            int mnAmount = rc.getResourceAmount(ResourceType.MANA);
+            if (mnAmount > 0) {
+                rc.transferResource(headquarter, ResourceType.MANA, mnAmount);
+            }
         }
     }
 
